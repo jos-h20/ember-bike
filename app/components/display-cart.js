6e623dc6e13c48078aafd39bcf23b939c0d0bff3
Josh Overly and Sara Wagner <josh@>
2016-04-06 11:07:56 -0700

    initial commit

---
+++ app/components/display-cart.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+
+  actions: {
+    remove(item) {
+      this.get('shoppingCart').remove(item);
+      }
+    },
+    empty() {
+      this.get('shoppingCart').empty([]);
+    }
+
+
+});
